4304581c717d76f475392933b102e7dd6beae5ba
骆代朋 <abc237274727@126.com>
2018-05-28 19:35:20 +0800

    添加其实评论编号参数

--- get_shops.py
+++ get_shops.py
@@ -4,20 +4,22 @@
 # @Author   : Edward  Luo<daipeng_luo@qq.com>
 # function  : 获取评论涉及到的店铺
 
+import os
+
 import config
 
 shop_list_path=config.shop_list_path
 comments_path=config.comments_path
 
 
-def get_shops():
-    shops = []
+def get_shops(start_comment):
+    shops = []  # 不重复的记录新增商家
     current_shops=get_current_shops()
     with open(shop_list_path, "a") as fa:
         with open(comments_path, "r") as fr:
             comments = fr.readlines()
-            for comment in comments:
-                comment_info = comment.strip("\n").split("\t")
+            for num in range(start_comment,len(comments)):
+                comment_info = comments[num].strip("\n").split("\t")
                 shop_id = comment_info[2]
                 shop_name = comment_info[3]
                 if shop_id in shops or shop_id in current_shops:
@@ -36,11 +38,14 @@ def get_current_shops():
     获取现有所有商户id
     :return:
     """
-    shops = []
+    if not os.path.exists(shop_list_path):
+        return []
+    cur_shops = []
     with open(shop_list_path, "r") as fr:
         for line in fr:
-            shops.append(line.strip("\n"))
-    return shops
+            cur_shops.append(line.strip("\n"))
+    return cur_shops
 
 if __name__ == '__main__':
-    get_shops()
+    start_comment=0     # 从第几个评论开始算起，由于所有评论数据都在一个文件里，当评论数据非常多以后，修改起始评论编号可以节约时间
+    get_shops(start_comment)
